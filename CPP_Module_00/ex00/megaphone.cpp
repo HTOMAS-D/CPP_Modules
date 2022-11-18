e34325cceaf8e076e754e8f41214136d151dab43
Henrique Tomas <henriquetomas@MBP-de-Henrique.lan>
2022-11-18 17:15:41 +0000

    first one done

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+int main(int ac, char **av){
+	std::string	str;
+	int i = 0;
+	int j;
+
+	if(ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else 
+	while (++i < ac)
+	{
+		j = -1;
+		str = av[i];
+		while(str[++j])
+			std::cout << (char) std::toupper(str[j]);
+		if((i + 1) < ac)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}

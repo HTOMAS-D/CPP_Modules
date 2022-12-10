071ce5a699b9b24a527fb478dff394c5c7ba11e7
Henrique Tomas <henriquetomas@MBP-de-Henrique.lan>
2022-12-10 20:17:17 +0000

    ex01 done

---
+++ CPP_Module_02/ex01/Fixed.hpp
@@ -0,0 +1,34 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <math.h>
+#include <iostream>
+
+/*******COLORS*******/
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define DEFAULT "\033[0m"
+
+class Fixed{
+	public:
+		Fixed(void); 							//Canonical
+		Fixed(Fixed const &copy);				//Canonical
+		Fixed &	operator=(Fixed const & rhs); 	//Canonical
+		Fixed(int const numb);
+		Fixed(float const numb);
+		~Fixed(void); 							//Canonical
+
+		float toFloat(void) const;
+		int toInt(void) const;
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+	private:
+		int _f_point;
+		static const int _f_bits;
+};
+
+std::ostream &operator<<(std::ostream &o, const Fixed &original);
+
+#endif

a2c0d760aa167706efd6b3342ac07bfb680f5d38
Henrique Tomas <henriquetomas@MacBook-Pro-de-Henrique.local>
2023-01-25 15:29:18 +0000

    some changes made

---
+++ CPP_Module_02/ex02/Fixed.cpp
@@ -0,0 +1,59 @@
+#include "Fixed.hpp"
+
+const int Fixed::_f_bits = 8;
+
+Fixed::Fixed(void){
+	std::cout << "Default constructor called!" << std::endl;
+	this->_f_point = 0;
+}
+
+Fixed::Fixed(Fixed const &copy){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::Fixed(int const numb){
+	std::cout << "Int constructor called!" << std::endl;
+	this->_f_point = numb * (1 << _f_bits); //get fixed number from int
+}
+
+Fixed::Fixed(float const numb){
+	std::cout << "Float constructor called!" << std::endl;
+	this->_f_point = roundf(numb * (1 << this->_f_bits)); //roundf  used to round the float to int
+}
+
+Fixed::~Fixed(void){
+	std::cout << "Deconstructor called!" << std::endl;
+}
+
+Fixed & Fixed::operator=(Fixed const &rhs){
+	std::cout <<"Assignement operator called!" << std::endl;
+	this->setRawBits(rhs.getRawBits());
+	return *this;
+}
+
+int Fixed::getRawBits(void) const{
+//	std::cout << "getRawBits called!" << std::endl;
+	return (this->_f_point);
+}
+
+void Fixed::setRawBits(int const raw){
+	this->_f_point = raw;
+}
+
+int Fixed::toInt(void)const{
+	return((this->_f_point >> this->_f_bits));
+}
+
+float Fixed::toFloat(void)const{
+	return((float)this->_f_point / (1 << this->_f_bits));
+}
+
+Fixed const& Fixed::max(Fixed const &a, Fixed const &b){
+
+}
+
+std::ostream &operator<<(std::ostream &o, const Fixed &original){
+	o << original.toFloat();
+	return (o);
+}
---
+++ CPP_Module_02/ex02/Fixed.hpp
@@ -0,0 +1,36 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <math.h>
+#include <iostream>
+
+/*******COLORS*******/
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define DEFAULT "\033[0m"
+
+class Fixed{
+	public:
+		//CONSTRUCTORS AND DECONSTRUCTORS
+		Fixed(void); 							//Canonical
+		Fixed(Fixed const &copy);				//Canonical
+		Fixed(int const numb);
+		Fixed(float const numb);
+		~Fixed(void); 							//Canonical
+
+		Fixed &	operator=(Fixed const & rhs); 	//Canonical
+		static Fixed const max(Fixed const a, Fixed const b);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+	private:
+		int _f_point;
+		static const int _f_bits;
+};
+
+std::ostream &operator<<(std::ostream &o, const Fixed &original);
+
+#endif

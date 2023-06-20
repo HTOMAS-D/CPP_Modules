c227bf84e739f8567b5b18042b1deee87b8fcc37
Ricky <henryats@gmail.com>
2023-06-20 17:16:50 +0100

    some changes

--- CPP_Module_04/ex03/Character.hpp
+++ CPP_Module_04/ex03/Character.hpp
@@ -9,15 +9,18 @@ class Character : public ICharacter
 	private:
 		std::string name;
 	public:
-	AMateria* inventory[4];
-	Character(std::string const & name);
+		Character(std::string const & name);
 		Character(Character const & src);
 		~Character();
 		Character & operator=(Character const & rhs);
+		
 		std::string const & getName() const;
+		
 		void equip(AMateria* m);
 		void unequip(int idx);
 		void use(int idx, ICharacter& target);
+		
+		AMateria* inventory[4];
 };
 
 #endif

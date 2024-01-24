a19bf01b8dfc476f19d2f6a0a840361afabdd0ed
Ricky <henryats@gmail.com>
2024-01-24 14:42:16 +0000

    last cpp08 done

---
+++ CPP_Module_08/ex02/main.cpp
@@ -0,0 +1,50 @@
+#include "MutantStack.hpp"
+#include <list>
+
+int main(){
+    MutantStack<int> mstack;
+    
+    mstack.push(5);
+    // mstack.push(17);
+    // std::cout << mstack.top() << std::endl;
+    // mstack.pop();
+    // std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+
+    while (it != ite){
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack); // testing if it can initialize a std::stack with the elements of mstack
+    // return 0;
+
+
+
+    std::cout << std::endl << std::endl << "NOW LISTS: " << std::endl << std::endl;
+    
+    std::list<int> list;
+    list.push_back(5);
+    list.push_back(3);
+    list.push_back(5);
+    list.push_back(737);
+    list.push_back(0);
+
+    std::list<int>::iterator ti = list.begin();
+    std::list<int>::iterator eti = list.end();
+    ++ti;
+    --ti;
+
+    while (ti != eti){
+        std::cout << *ti << std::endl;
+        ++ti;
+    }
+    
+    return 0;
+}

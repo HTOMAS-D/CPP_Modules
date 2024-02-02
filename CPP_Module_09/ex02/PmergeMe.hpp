ff35204b8aeefeb76e13db9c3ecae00571adc248
Ricky <henryats@gmail.com>
2024-02-02 18:26:33 +0000

    cpp09 done

--- CPP_Module_09/ex02/PmergeMe.hpp
+++ CPP_Module_09/ex02/PmergeMe.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <sys/time.h>
 
 class PmergeMe{
     private:
@@ -10,6 +11,7 @@ class PmergeMe{
         int _ac;
         char **_av;
         std::vector<int> _vector;
+        std::vector<int> _vectorDone;
         std::list<int> _list;
 
     public:
@@ -19,4 +21,13 @@ class PmergeMe{
         void checkArgs();
         void makeContainers();
         void printer(int flag);
+        long double getTime();
+
+        std::vector<int> mergeVec(std::vector<int> &v);
+        std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right);
+
+        std::list<int> mergeList(std::list<int> &lst);
+        std::list<int> merge(const std::list<int>& left, const std::list<int>& right);
+
+
 };
